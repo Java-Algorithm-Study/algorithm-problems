4f12aad00b4df5f4ba91ee5babc6441b75e0276b
banan99 <banan99@naver.com>
2022-02-28 15:43:52 +0900

    code : yoonji boj 9012

--- yoonji/home/2.28/boj_9012.java
+++ yoonji/home/2.28/boj_9012.java
@@ -4,35 +4,33 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class boj_9012 {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); // 입출력을 위한 BufferedReader 선언 
-		int N = Integer.parseInt(bf.readLine()); // 읽어올 라인의 갯수 
-		for (int i = 0; i < N; i++) {
-			String answer = "YES";
-			Stack<Integer> stack = new Stack<>();
-			String parenthesis = bf.readLine(); // 각 라인 
-			char[] symbols = parenthesis.toCharArray(); // 라인의 문자들을 char로 쪼개 배열로 저장 
-			for (char symbol : symbols) {
-				// 여는 괄호 '(' 가 나오면 스택에 푸시한
-				if (symbol == '(') {
-					stack.push(0);
-				} else if (symbol == ')') {
-					if (stack.empty()) {
-						// 닫는 괄호가 나왔을 때 스택이 비어있으면 오류 출력 
-						answer = "NO";
-					} else {
-						// 닫는 괄호가 나왔을 때 스택이 차 있으면 스택에서 팝한다 
-						stack.pop();
-					}
+	private static void solving() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		int sum = 0;
+		String[] parenthesis;
+		// 테스트 케이스만큼 반복
+		for (int i = 0; i < T; i++) {
+			parenthesis = br.readLine().split("");
+			for (String p : parenthesis) {		// "("를 만나면 +1, ")"를 만나면 -1
+				if (p.equals("(")) {
+					sum += 1;
+				}
+				else if (p.equals(")")) {
+					sum -= 1;
+				}
+				if (sum < 0) {  // ')'가 더 많은 경우
+					System.out.println("NO");
+					break;
 				}
 			}
-			// 입력의 끝에 스택이 비어 있지 않다면 오류를 출력한다 
-			if (!stack.empty()) {
-				answer = "NO";
-			}
-			System.out.println(answer);
+			if (sum == 0) System.out.println("YES");
+			else if (sum > 0) System.out.println("NO");
+			sum = 0;
 		}
-		
+	}
+
+	public static void main(String[] args) throws IOException {
+		solving();
 	}
 }
