0b8120e7f03eb62c04db33456f339d9d6d092968
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-03-04 03:01:17 +0900

    programmers 숫자 문자열과 영단어

---
+++ chris-an/office/3.03/Programmers_81301.java
@@ -0,0 +1,22 @@
+/*
+
+class Solution {
+
+    public int solution(String s) {
+
+    String [] initArray = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    String [] arArray = {"1","2","3","4","5","6","7","8","9"};
+        int answer;
+        for(int i = 0; i < initArray.length; i++) {
+            if(s.contains(initArray[i])) {
+                s = s.replace(initArray[i], arArray[i]);
+
+            }
+        }
+        answer = Integer.parseInt(s);
+
+        return answer;
+    }
+}
+
+*/
