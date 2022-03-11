0f1e228a152fa2524e6ab2c483ad3d618ff9bd0f
junggeehoon <junggeehoon@gmail.com>
2022-03-11 13:07:59 +0900

    파일 디렉토리 수정

--- jihoon/home/3-7/Boj_10820.java
+++ jihoon/home/3-7/Boj_10820.java
@@ -8,7 +8,7 @@ public class Boj_10820 {
         String line;
         while ((line = bf.readLine()) != null) {
             int[] sum = new int[4];
-            String[] ans = new String[4];
+            var answer = new StringBuffer(4);
 
             // sum 배열에 순서대로 소문자, 대문자, 숫자, 공백 갯수를 넣어준다
             for (int i = 0; i < line.length(); i++) {
@@ -24,13 +24,14 @@ public class Boj_10820 {
                 }
             }
 
-            // sum 배열을 문자로 만들어 준다
-            for (int i = 0; i < sum.length; i++) {
-                ans[i] = String.valueOf(sum[i]);
+            for (int num : sum) {
+                answer.append(String.valueOf(num));
+                answer.append(" ");
             }
 
-            System.out.println(String.join(" ", ans));
+            System.out.println(answer);
         }
+
 
     }
 }
--- jihoon/home/3-9/Boj_2609.java
+++ jihoon/home/3-9/Boj_2609.java
@@ -4,6 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Boj_2609 {
+
+    // 유클리드 호제법
     private static int gcd(int a, int b) {
         while (b != 0) {
             int r = a % b;
@@ -13,6 +15,19 @@ public class Boj_2609 {
         return a;
     }
 
+    private static int gcd_slow(int a, int b) {
+        int smallNumber = Math.min(a, b);
+        int bigNumber = Math.max(a, b);
+
+        for (int i = smallNumber; i > 0; i--) {
+            if (bigNumber % i == 0 && smallNumber % i == 0) {
+                return i;
+            }
+        }
+        return 1;
+    }
+
+    // 두 자연수 a, b에 대하여 최소 공배수는 두 수의 곱을 최대공약수로 나눈 값과 같다
     private static int lcm(int a, int b) {
         return (a * b) / gcd(a, b);
     }
@@ -23,8 +38,10 @@ public class Boj_2609 {
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
+        int gcd = gcd(a, b);
+        int lcm = (a * b) / gcd;
 
-        System.out.println(gcd(a, b));
-        System.out.println(lcm(a, b));
+        System.out.println(gcd);
+        System.out.println(lcm);
     }
 }
