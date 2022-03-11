13fc79d9087f0c06b42ad1111f7925964cc78ff7
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-03-11 18:53:13 +0900

    code : Boj 1676

--- chris-an/office/3.10/Programmers_42842.java
+++ chris-an/office/3.10/Programmers_42842.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 public class Programmers_42842 {
     
     public static int [] solution(int brown, int yellow) {
+        /*
         int a = brown/2 + 2;
         int b = yellow + brown;
 
@@ -12,9 +13,20 @@ public class Programmers_42842 {
         double beta = a - alpha;
 
         int [] answer = {(int) alpha, (int) beta};
+        */
+
+        int[] answer = new int[2];
+
+        for(int i = 1; i <= yellow; i++) {
+            if(yellow % i == 0) {
+                if(2 * i + (2 * yellow / i) == brown - 4) {
+                    answer[0] = i + 2;
+                    answer[1] = yellow / i + 2;
+                }
+            }
+        }
         return answer;
     }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String [] ch = br.readLine().split(" ");
