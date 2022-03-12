4570995081d10a78eabb3c8dd0b929ff1a6afa27
banan99 <banan99@naver.com>
2022-03-12 16:07:31 +0900

    refactoring: 불필요한 if문과 배열 생성 제거

--- yoonji/home/3/12/boj_17087.java
+++ yoonji/home/3/12/boj_17087.java
@@ -9,22 +9,12 @@ public class boj_17087 {
         String[] NS = br.readLine().split(" ");
         int N = Integer.parseInt(NS[0]);
         int S = Integer.parseInt(NS[1]);
-        int[] diff = new int[N];
         String[] A = br.readLine().split(" ");
 
-        for (int i = 0; i < N; i++) {
-            diff[i] = Math.abs(S - Integer.parseInt(A[i]));
-        }
-
-        int ans = diff[0];
-        // 동생이 1명인 경우, 그냥 리턴
-        if (N == 1) {
-            System.out.println(diff[0]);
-            return;
-        }
+        int ans = Math.abs(S - Integer.parseInt(A[0]));
         // 최대공약수 구하기
         for (int i=1; i<N; i++) {
-            ans = solveGCD(ans, diff[i]);
+            ans = solveGCD(ans, Math.abs(S - Integer.parseInt(A[i])));
         }
         System.out.println(ans);
     }
