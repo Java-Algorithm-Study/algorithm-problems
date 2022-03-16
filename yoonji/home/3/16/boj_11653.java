6beda3cf6695654123302321842beddd0ee2ff27
banan99 <banan99@naver.com>
2022-03-16 18:27:36 +0900

    code: boj 11653 소인수분해

--- yoonji/home/3/16/boj_11653.java
+++ yoonji/home/3/16/boj_11653.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class boj_11653 {
+import java.util.Scanner;
+// 소인수 분해
+public class boj_11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int input = sc.nextInt();
+        for (int i=2; i<=input; i++) {
+            if (input == 1) break;
+            if (input % i == 0) {
+                sb.append(i).append("\n");
+                input = input / i;
+                i--;
+            }
+        }
+        /*
+        while (input != 1) {
+            for (int i=2; i<=input; i++) {
+                if (input % i == 0) {
+                    sb.append(i).append("\n");
+                    input = input / i;
+                    break;  // 다시 2부터
+                }
+            }
+        }
+         */
+        System.out.println(sb);
+    }
 }
