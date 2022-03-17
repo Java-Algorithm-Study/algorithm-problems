6804907cccc5d72d5b06b8d414cc5ed72a3b657d
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-03-17 22:23:53 +0900

    code : Programmers 문자열 내마음대로 정렬하기

--- chris-an/office/3.17/Pro_12915.java
+++ chris-an/office/3.17/Pro_12915.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Pro_12915 {
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Pro_12915 {
+    // 문자열 내마음대로 정렬하기
+    public String[] solution(String[] strings, int n) {
+
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                int num = a.charAt(n) - b.charAt(n);
+
+                // 예제 2번 입출력 참고. (문자가 같을 경우 처리)
+                if(a.charAt(n) == b.charAt(n)) return a.compareTo(b);
+                else return num;
+            }
+        });
+        return strings;
+    }
+    public static void main(String[] args) {
+
+    }
 }
