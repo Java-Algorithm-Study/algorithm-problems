51f4694dd3fa25d2d772008f28c45c74547a3326
Jeongwoo Yang <63606128+Jeong-w-o-o@users.noreply.github.com>
2022-03-18 23:03:33 +0900

    [FEAT] BOJ 11726 2xn 타일링

--- jeongwoo/home/2022-03-17/Boj11726.java
+++ jeongwoo/home/2022-03-17/Boj11726.java
@@ -14,6 +14,10 @@ import java.io.InputStreamReader;
  * -시간 복잡도
  * 1. O(N) 이하..?
  *
+ * -틀린 이유
+ * 1. dp[1], dp[2]를 초기화하고 시작했는데, 이 경우 dp = new int[n+1]로 잡아 줬기 때문에 n = 1인 경우 out ouf bounds
+ * 2. 해결 방법 1: dp = new int[1001]
+ * 3. 해결 방법 2: dp[0] = dp[1] = 1로 초기화하고, for문을 2부터 시작.
  */
 
 public class Boj11726 {
@@ -21,16 +25,14 @@ public class Boj11726 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] dp = new int[n+1];
+        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = 2;
         int mod = 10007;
 
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
         }
 
         System.out.println(dp[n]);
-
-
     }
 }
