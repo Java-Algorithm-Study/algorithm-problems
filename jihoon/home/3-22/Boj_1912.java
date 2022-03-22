65df406f5df9b0f06451e64420cde9085af55f56
junggeehoon <junggeehoon@gmail.com>
2022-03-22 20:59:53 +0900

    boj 1912

---
+++ jihoon/home/3-22/Boj_1912.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_1912 {
+    public static void main(String[] args) throws IOException {
+        var bf = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bf.readLine());
+        String[] temp = bf.readLine().split(" ");
+        int[] seq = new int[N];
+        int[] dp = new int[N];
+        
+        for (int i = 0; i < temp.length; i++)
+            seq[i] = Integer.parseInt(temp[i]);
+    
+        dp[0] = seq[0];
+        for (int i = 1; i < N; i++) {
+            dp[i] = seq[i];
+            dp[i] = Math.max(dp[i - 1] + dp[i], dp[i]);
+        }
+    
+        int max = Integer.MIN_VALUE;
+        for (int num : dp)
+            if (num > max) max = num;
+        System.out.println(max);
+    }
+}
