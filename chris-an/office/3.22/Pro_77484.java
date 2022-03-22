deb00ad19baf3b6075a6524e177226f9efb77c38
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-03-22 20:16:22 +0900

    code : Programmers 약수의 개수와 덧셈

---
+++ chris-an/office/3.22/Pro_77484.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int getNumber(int n) {
+        int cnt = 0;
+        for (int i = 1; i * i <= n; i++) {
+            if (n % i == 0) {
+                cnt++;
+                if (i * i != n)
+                    cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for (int i = left; i <= right; i++) {
+            if(getNumber(i) % 2 == 0) answer += i;
+            else answer -= i;
+        }
+        return answer;
+    }
+}
