3aa281de111acd5a0556f84ce1dd419f8339d37c
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-03-22 20:20:07 +0900

    code : Proragmmers 3진법뒤집기

---
+++ chris-an/office/3.22/Pro_68935.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Pro_68935 {
+    public int solution(int n) {
+        int answer = 0;
+        Stack<Integer> stk = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        while(n != 0) {
+            stk.add(n % 3);
+            n = n / 3;
+        }
+
+        int i = 0;
+        while (!stk.isEmpty()) {
+            answer +=  stk.pop() * Math.pow(3, i++);
+        }
+
+        return answer;
+    }
+}
