09cd3baf7fd094463d7fe955e43c4b8ea872eec4
banan99 <banan99@naver.com>
2022-03-30 18:04:36 +0900

    code: prg 예산

--- yoonji/office/3/29/prg_예산.java
+++ yoonji/office/3/29/prg_예산.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class prg_예산 {
+import java.util.Arrays;
+
+class prg_예산 {
+    public int solution(int[] d, int budget) {
+        // 1. Sort
+        Arrays.sort(d);
+
+        // 2. 적은 금액부터 지원금++
+        int sum = 0;
+        int cnt = 0;
+        for (int i=0; i<d.length; i++) {
+            sum+=d[i];
+            if (sum <= budget) cnt++;
+        }
+        return cnt;
+    }
 }
