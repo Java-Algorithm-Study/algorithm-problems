c32bad8bb961d875f447308de189aff892030131
junggeehoon <junggeehoon@gmail.com>
2022-03-31 22:38:21 +0900

    프로그래머스 이진변환 반복하기

---
+++ jihoon/office/3-31/Pro_70129.java
@@ -0,0 +1,33 @@
+public class Pro_70129 {
+    
+    public static int[] solution(String s) {
+        int[] answer = new int[2];
+        int countExecution = 0;
+        int countZero = 0;
+        
+        while (!s.equals("1")) {
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '0')
+                    countZero++;
+            }
+            s = toBinary(s);
+            countExecution++;
+        }
+        answer[0] = countExecution;
+        answer[1] = countZero;
+        return answer;
+    }
+    
+    public static String toBinary(String s) {
+        int numberOf1 = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1')
+                numberOf1++;
+        }
+        return Integer.toBinaryString(numberOf1);
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(solution("110010101001"));
+    }
+}
