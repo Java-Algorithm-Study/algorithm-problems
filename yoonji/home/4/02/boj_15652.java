672a476b24fecf2973e02d9b968a843ca7db8b89
banan99 <banan99@naver.com>
2022-04-02 23:53:09 +0900

    code: boj 15652 N과M(4)

---
+++ yoonji/home/4/02/boj_15652.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+// N과 M (4)
+// 조건 : 증가순, 비내림차순 (전 값보다 같거나 큰 값이 나와야 한다), 중복 허용
+public class boj_15652 {
+    static int N, limit;
+    static int[] line;
+    static StringBuilder answerSB = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        limit = Integer.parseInt(st.nextToken());
+        line = new int[limit];
+        dfs(0, 1);
+        System.out.println(answerSB);
+    }
+
+    private static void dfs(int depth, int num) {
+        if (depth == limit) {
+            for (int i:line) {
+                answerSB.append(i).append(" ");
+            }
+            answerSB.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {   // 여기서 반복문을 돌려야 됨
+            if (depth>0 && line[depth-1] > i+1) continue;    // 조건 수행, 그 전 depth의 값보다 현재 값이 작으면 넣지 않고 pass
+            line[depth] = i+1;  // 배열의 index는 depth와 동일
+            dfs(depth + 1, i + 1);
+        }
+    }
+}
