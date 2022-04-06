41900bf77fcd45321ff0d42bc39c969088897016
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-04-06 17:26:30 +0900

    code : Boj 15665 N과M(11)

---
+++ chris-an/home/4.06/Boj_15665.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Boj_15665 {
+    static int N, M;
+    static int[] arr;
+    static int[] inputDataArr;
+    static StringBuilder sb = new StringBuilder();
+
+    static void backtracking(int depth) throws IOException {
+        if (depth == M) {
+            for (int i : arr) {
+                sb.append(i).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        int beforeV = 0; // sort 를 하니깐, 동일한 숫자 값이 전 idx로 확인 가능합니다.
+        for (int i = 0; i < N; i++) {
+            if (beforeV == inputDataArr[i]) continue;
+
+            arr[depth] = inputDataArr[i];
+            beforeV = arr[depth];
+
+            backtracking(depth + 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+
+        inputDataArr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            inputDataArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(inputDataArr);
+
+        backtracking(0);
+
+        System.out.println(sb);
+    }
+}
