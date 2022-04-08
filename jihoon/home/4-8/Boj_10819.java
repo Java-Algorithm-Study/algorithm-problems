7d83272c81d713e8342c29536b2529407c5c5151
junggeehoon <junggeehoon@gmail.com>
2022-04-08 10:54:18 +0900

    boj 10819

---
+++ jihoon/home/4-8/Boj_10819.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_10819 {
+    public static int[] numbers;
+    public static int N;
+    public static int[] arr;
+    public static boolean[] visit;
+    public static int ans = 0;
+    
+    public static void bfs(int depth) {
+        if (depth == N) {
+            int sum = 0;
+            for (int i = 1; i < arr.length; i++) {
+                sum += Math.abs(arr[i - 1] - arr[i]);
+            }
+            if (sum > ans) ans = sum;
+            return;
+        }
+    
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = numbers[i];
+                bfs(depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        var bf = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(bf.readLine());
+        arr = new int[N];
+        visit = new boolean[N];
+        numbers = new int[N];
+        
+        var st = new StringTokenizer(bf.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+    
+        bfs(0);
+        
+        System.out.println(ans);
+    }
+}
