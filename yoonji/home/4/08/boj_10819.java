c8de835618fb6eaf3a817a5e2d66b250730ffc0f
banan99 <banan99@naver.com>
2022-04-08 23:49:25 +0900

    code: boj 10819 차이를 최대로

--- yoonji/home/4/08/boj_10819.java
+++ yoonji/home/4/08/boj_10819.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class boj_10819 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 차이를 최대로
+public class boj_10819 {
+    private static int max = Integer.MIN_VALUE;
+    private static int sum = 0;
+    private static int[] line;
+    private static boolean[] visited;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] nums = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i=0; i<N; i++)
+            nums[i] = Integer.parseInt(st.nextToken());
+        visited = new boolean[N];
+        line = new int[N];
+        dfs(0, N, nums);
+        System.out.println(max);
+    }
+
+    public static void dfs(int depth, int N, int[] nums) {
+        if (depth == N) {
+            int sum = 0;
+            for (int i=0; i<N-1; i++) sum+= Math.abs(line[i] - line[i+1]);  // 차이의 절댓값
+            max = Math.max(max, sum);   // 최댓값
+        }
+        for (int i=0; i<N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                line[depth] = nums[i];
+                dfs(depth+1, N, nums);
+                visited[i] = false;
+            }
+        }
+    }
 }
