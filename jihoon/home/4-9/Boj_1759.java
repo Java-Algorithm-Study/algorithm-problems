1d202e28484f7b9bfbcb492fd57e73b539fd40b7
junggeehoon <junggeehoon@gmail.com>
2022-04-09 13:31:07 +0900

    boj 1759

---
+++ jihoon/home/4-9/Boj_1759.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Boj_1759 {
+    public static int L;
+    public static int C;
+    public static String[] s;
+    public static String[] arr;
+    public static boolean[] visit;
+    public static StringBuilder sb = new StringBuilder();
+    public static final List<String> vowels = Arrays.asList("a", "e", "i", "o", "u");
+    
+    public static void dfs(int at, int depth) {
+        if (depth == L) {
+            int countVowels = countVowels(arr);
+            if (countVowels > 0 && countVowels <= L - 2) {
+                for (String str : arr) {
+                    sb.append(str);
+                }
+                sb.append("\n");
+            }
+            
+            return;
+        }
+    
+        for (int i = at; i < C; i++) {
+            if (visit[i]) continue;
+            visit[i] = true;
+            arr[depth] = s[i];
+            dfs(i, depth + 1);
+            visit[i] = false;
+        }
+    }
+    
+    public static int countVowels(String[] s) {
+        int count = 0;
+        for (String str : s)
+            if (vowels.contains(str)) count++;
+        
+        return count;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        var bf = new BufferedReader(new InputStreamReader(System.in));
+        var st = new StringTokenizer(bf.readLine(), " ");
+        var line = new StringTokenizer(bf.readLine(), " ");
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        s = new String[C];
+        arr = new String[L];
+        visit = new boolean[C];
+    
+        for (int i = 0; i < C; i++) {
+            s[i] = line.nextToken();
+        }
+        Arrays.sort(s);
+        
+        dfs(0, 0);
+        System.out.println(sb);
+        bf.close();
+    }
+}
