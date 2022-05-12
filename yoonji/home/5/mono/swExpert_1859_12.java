797e8d9684e87ef0ed59650d91aac6a5f8787113
banan99 <banan99@naver.com>
2022-05-12 23:47:23 +0900

    [FEAT] sw 1859

--- yoonji/home/5/mono/swExpert_1859_12.java
+++ yoonji/home/5/mono/swExpert_1859_12.java
@@ -1,2 +1,27 @@
-public class swExpert_1859 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class swExpert_1859_12 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T;
+        T= br.read();
+
+        for(int test_case = 1; test_case <= T; test_case++) {
+            int N = br.read();
+            int[] price = new int[N];
+            int sum = 0;
+            String[] tmp = br.readLine().split(" ");
+
+            for (int i=0; i<tmp.length;i++) {
+                price[i] = Integer.parseInt(tmp[i]);
+            }
+            int max = price[price.length-1];
+            for (int i=price.length-2; i>=0; i--) {
+                if (price[i] > max) max = price[i];
+                else if (price[i] < max) sum += max - price[i];
+            }
+            System.out.println("#"+test_case+" "+sum);
+        }
+    }
 }
