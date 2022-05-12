26e239b0b2b08e9da68cbf498bb19b0a8c586848
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-05-13 01:37:07 +0900

    diff2 - 백만장자 프로젝트(1859)

---
+++ chris-an/home/5.12/Sw_1859.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Sw_1859 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        // input Value Setting
+        for (int i = 0; i < T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] inputData = new int[N];
+            for (int j = 0; j < N; j++) {
+                inputData[j] = Integer.parseInt(st.nextToken());
+            }
+            int max = Integer.MIN_VALUE;
+            long result = 0;
+            for (int k = N-1; k >= 0; k--) {
+                if (max > inputData[k]) {
+                    result += max - inputData[k];
+                } else {
+                    max = inputData[k];
+                }
+            }
+            System.out.println("#" + (i+1) + " " + result);
+        }
+    }
+}
