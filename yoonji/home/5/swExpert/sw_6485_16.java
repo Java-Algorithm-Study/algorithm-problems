d9ce7ee8f6d1b10091d5249980f32d179b6606d9
banan99 <banan99@naver.com>
2022-05-18 00:04:10 +0900

    [REFACTOR] print메서드 수정

--- yoonji/home/5/swExpert/sw_6485_16.java
+++ yoonji/home/5/swExpert/sw_6485_16.java
@@ -18,21 +18,27 @@ public class sw_6485_16 {
                 busLine[i][1] = strToInt(line.split(" ")[1]);
             }
             int busStopNum = strToInt(br.readLine());
-            int[] busStop = new int[busStopNum];
+            int[] busStop = new int[busStopNum+1];
             for (int i=0; i<busStopNum; i++) {
                 busStop[i] = strToInt(br.readLine());
             }
-            int[] answer = new int[busStopNum];
+            int[] answer = new int[5000+1];   // index가 정류장 번호니까
             for (int i=0; i<N; i++) {
                 int start = busLine[i][0];
                 int end = busLine[i][1];
-                for (int p=0; p<busStopNum; p++) {
+                for (int p=0; p < busStopNum; p++) {
                     if (start<=busStop[p] && busStop[p]<=end) answer[p]++;
+                    //busStop { 10, 4, 30, 96, 100
+//                    p 0 -> busStop[0]==10 == 1 -> answer[0] ++;
+//                    p 1 -> busStop[1]==4 != 1
+//                    p 2 -> busStop[2]==30 != 1
+//                    p 3 -> busStop[3]==96 != 1
+//                    p 4 -> busStop[4]==100 != 1
                 }
             }
             StringBuilder sb = new StringBuilder();
             for (int i=0; i<busStopNum; i++) sb.append(answer[i]).append(" ");
-            System.out.print("#"+ tc + " " + sb);
+            System.out.println("#"+ tc + " " + sb);
         }
     }
     private static int strToInt(String str) { return Integer.parseInt(str); }
