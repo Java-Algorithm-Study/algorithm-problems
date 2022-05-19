c02b94ea5ab6ef781766e7e516d4a07dc5301a90
junggeehoon <junggeehoon@gmail.com>
2022-05-19 16:44:27 +0900

    프로그래머스 h-index

---
+++ jihoon/office/5-19/Pro_42747.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Pro_42747 {
+    public static int solution(int[] citations) {
+        Arrays.sort(citations);
+        int max = 0;
+        for (int i = 0; i <= citations[citations.length / 2]; i++) {
+            for (int j = 0; j < citations.length; j++) {
+                if (citations[j] >= i) {
+                    if (citations.length - j >= i && j + 1 <= i) {
+                        max = Math.max(i, max);
+                    }
+                }
+            }
+        }
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        int[] citations = {0, 7, 8, 9, 10, 11, 12, 13, 14};
+        System.out.println(solution(citations));
+    }
+}
