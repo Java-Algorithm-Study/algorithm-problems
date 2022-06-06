96dbe804e3d28e7eec9eaa1dd263742ea48b58ea
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-06-06 09:13:06 +0900

    code : Boj 10816 성공

--- chris-an/home/6.06/Boj_10816_pass.java
+++ chris-an/home/6.06/Boj_10816_pass.java
@@ -1,55 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Boj_10816 {
+public class Boj_10816_pass {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        ArrayList<Integer> numberCardArr = new ArrayList<>();
+        int[] numberCardArr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) numberCardArr.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N; i++) numberCardArr[i] = Integer.parseInt(st.nextToken());
 
         int M = Integer.parseInt(br.readLine());
-        ArrayList<Integer> keyArr = new ArrayList<>();
+        int[] keyArr = new int[M];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) keyArr.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < M; i++) keyArr[i] = Integer.parseInt(st.nextToken());
 
-        Collections.sort(numberCardArr);
-        int[] result = new int[M];
-        for (int i = 0; i < M; i++) {
-            int lowIdx = lowerBound(keyArr.get(i), 0, numberCardArr.size(), numberCardArr);
-            int highIdx = upperBound(keyArr.get(i), 0, numberCardArr.size(), numberCardArr);
-            result[i] = highIdx - lowIdx;
-        }
-
+        Arrays.sort(numberCardArr);
         StringBuilder sb = new StringBuilder();
-        for (int i : result) {
-            sb.append(i).append(' ');
+        for (int i = 0; i < M; i++) {
+            int lowIdx = lowerBound(keyArr[i], 0, numberCardArr.length, numberCardArr);
+            int highIdx = upperBound(keyArr[i], 0, numberCardArr.length, numberCardArr);
+            sb.append(highIdx - lowIdx).append(' ');
         }
+
         System.out.println(sb);
     }
 
-    static int lowerBound(int key, int low, int high, ArrayList<Integer> numberCardArr) {
+    // 하한선
+    static int lowerBound(int key, int low, int high, int[] numberCardArr) {
         int mid;
         while (low < high) {
             mid = (low + high) / 2;
-            if (key <= numberCardArr.get(mid)) high = mid;
+            if (key <= numberCardArr[mid]) high = mid;
             else low = mid + 1;
         }
         return low;
     }
-
-    static int upperBound(int key, int low, int high, ArrayList<Integer> numberCardArr) {
+    // 상한선
+    static int upperBound(int key, int low, int high, int[] numberCardArr) {
         int mid;
         while (low < high) {
             mid = (low + high) / 2;
-            if (key < numberCardArr.get(mid)) high = mid;
+            if (key < numberCardArr[mid]) high = mid;
             else low = mid + 1;
         }
         return low;
