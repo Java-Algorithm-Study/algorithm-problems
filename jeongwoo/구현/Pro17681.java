adaa12ef2000a0e139a2080b40edbc9cf853b680
Jeongwoo Yang <63606128+Jeong-w-o-o@users.noreply.github.com>
2022-06-23 23:32:14 +0900

    [FEAT] 2022-03-31 프로그래머스 17681 1차 비밀지도

--- jeongwoo/구현/Pro17681.java
+++ jeongwoo/구현/Pro17681.java
@@ -7,35 +7,29 @@
  * 2. 변환한 것끼리 비트 OR 연산 진행
  * 3. 1인 곳들을 #으로 해 줌
  *
- * -시간 복잡도
- * 1.
- *
- * -자료 구조
- * 1.
  */
 
 public class Pro17681 {
-    static public String[] solution(int n, int[] arr1, int[] arr2) {
+     public String[] solution(int n, int[] arr1, int[] arr2) {
 
-        String [] answer = new String[n];
+        String[] answer = new String[n];
 
         for (int i = 0; i < n; i++) {
             answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
-
         }
 
         for (int i = 0; i < n; i++) {
             int zeroCnt = n - answer[i].length();
             String zero = "";
-            while(zeroCnt-- > 0) {
+            while (zeroCnt-- > 0) {
                 zero += "0";
             }
             answer[i] = zero + answer[i];
         }
 
         for (int i = 0; i < n; i++) {
-            answer [i] = answer[i].replace('1', '#');
-            answer [i] = answer[i].replace('0', ' ');
+            answer[i] = answer[i].replace('1', '#');
+            answer[i] = answer[i].replace('0', ' ');
         }
 
         return answer;
