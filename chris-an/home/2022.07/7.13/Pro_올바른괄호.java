8cb78f1f10b0431d07bcbfd971a1d3a3000ed25e
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-07-13 21:56:35 +0900

    code : Prg 올바른 괄호

---
+++ chris-an/home/2022.07/7.13/Pro_올바른괄호.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class Pro_올바른괄호 {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stk = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                stk.push(c);
+            }else {
+                if(stk.isEmpty()) return false;
+                if(stk.peek() == '(') {
+                    stk.pop();
+                }else {
+                    stk.push(c);
+                }
+            }
+        }
+
+        if(!stk.isEmpty()) answer = false;
+        return answer;
+    }
+}
