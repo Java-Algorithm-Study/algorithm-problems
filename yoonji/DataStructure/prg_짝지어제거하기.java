8af22360c1313cab845e1576796c105a738cf739
banan99 <banan99@naver.com>
2022-07-29 10:53:27 +0900

    [FEAT] prg 짝지어제거하기

--- yoonji/DataStructure/prg_짝지어제거하기.java
+++ yoonji/DataStructure/prg_짝지어제거하기.java
@@ -1,19 +1,21 @@
 package DataStructure;
 
 import java.util.Stack;
-
+//알파벳 소문자 문자열 (같은 알파벳 2개 제거)
+// 성공 1, 실패 0
+// 안에 없으면 넣기. 있으면 top 체크
+// 먼저 들어간게 나중에 나온다. 후입선출 => ...스택
 // Stack 사용해야 효율성 테스트에서 통과
 public class prg_짝지어제거하기 {
     public int solution(String s) {
         Stack<Character> stack = new Stack<>();
-        for (int i=0; i<s.length(); i++) {
-            char c = s.charAt(i);
-            if (!stack.isEmpty() && c == stack.peek()) {
+        for (char word : s.toCharArray()) {
+            if (!stack.isEmpty() && word == stack.peek()) {
                 stack.pop();
             } else {
-                stack.push(c);
+                stack.push(word);
             }
         }
-        return stack.size() == 0? 1: 0;
+        return stack.isEmpty()? 1: 0;
     }
 }
