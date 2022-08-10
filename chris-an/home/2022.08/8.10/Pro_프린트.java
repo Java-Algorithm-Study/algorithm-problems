a9cfdc7f1138b5be280c268279f4b0ef7e2152b0
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-08-10 17:35:13 +0900

    code : Prg 프린트

---
+++ chris-an/home/2022.08/8.10/Pro_프린트.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Pro_프린트 {
+    public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 1;
+
+        for (int i = 0; i < priorities.length; i++) {
+            pq.offer(priorities[i]);
+        }
+
+        while (!pq.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (priorities[i] == pq.peek()) {
+                    if (i == location) {
+                        return answer;
+                    }
+                    pq.poll();
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
