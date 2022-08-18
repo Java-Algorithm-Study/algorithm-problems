6873cd2df821fa9c83dc9e7a0acc7a4a5d555013
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2022-08-18 11:12:03 +0900

    code : Prg 점프와 순간이동

---
+++ chris-an/home/2022.08/8.18/Pro_점프와순간이동.java
@@ -0,0 +1,13 @@
+public class Pro_점프와순간이동 {
+    public int solution(int n) {
+        int ans = 0;
+        while(n != 0) { // 0이면 종료
+            if(n % 2 == 1) {
+                ans++;
+                n--;
+            }
+            n/=2;
+        }
+        return ans;
+    }
+}
