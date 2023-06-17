8644617ed5a06d27a417fb2d2bd35bcfa4526dab
JUNGMIN AN <71064490+CHRIS-AN@users.noreply.github.com>
2023-06-17 23:02:34 +0900

    leetcode - Palindrome Permutation

--- chris-an/src/main/kotlin/leetcode/leetcode_0617_2.kt
+++ chris-an/src/main/kotlin/leetcode/leetcode_0617_2.kt
@@ -1,4 +1,27 @@
 package main.kotlin.leetcode
 
+/**
+ * Palindrome Permutation
+ * https://leetcode.com/problems/palindrome-permutation/description/?envType=study-plan-v2&envId=premium-algo-100
+ */
 class leetcode_0617_2 {
+    fun canPermutePalindrome(s: String): Boolean {
+        var map = HashMap<Char, Int>()
+        for (c in s) {
+            map[c] = (map[c] ?: 0) + 1
+        }
+        var oddCount = 0
+        for (v in map.values) {
+            if (v % 2 == 1) oddCount++
+        }
+        return oddCount <= 1
+    }
+}
+
+fun main() {
+    println(
+        leetcode_0617_2().canPermutePalindrome(
+            "aab"
+        )
+    )
 }
